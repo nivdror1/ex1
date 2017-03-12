3ce35639cedd70fbf5c20e2987b0fdc0c24e9e3b
nivdror1 <nivdror1@gmail.com>
2017-03-12 17:45:55 +0200

    init

--- osm.cpp
+++ osm.cpp
@@ -1,12 +1,12 @@
-#ifndef _OSM_H
-#define _OSM_H
 
 #include <unistd.h>
+#include <iostream>
+#include <sys/time.h>
+#include "osm.h"
 
-/* calling a system call that does nothing */
-#define OSM_NULLSYSCALL asm volatile( "int $0x80 " : : \
-        "a" (0xffffffff) /* no such syscall */, "b" (0), "c" (0), "d" (0) /*:\
-        "eax", "ebx", "ecx", "edx"*/)
+#define MAX_MACHINE_NAME_CHAR 255
+
+
 
 
 /* Initialization function that the user must call
@@ -17,16 +17,7 @@
  * Returns 0 upon success and -1 on failure
  */
 int osm_init(){
-	try{
-		timeMeasurmentStructure time= new timeMeasurmentStructure();
-		if(getHostname(time.machineName,255)==-1){
-			std::cout<<"bad host name"<<endl; //todo check out what to do
-		}
-		return 0;
-	}catch (std::bad_alloc e){
-		e.printstack();
-		return -1;
-	}
+
 }
 
 
@@ -34,9 +25,10 @@ int osm_init(){
  * after running any other library function.
  * The function may, for example, free memory or
  * close/delete files.
- * Returns 0 uppon success and -1 on failure
+ * Returns 0 upon success and -1 on failure
  */
-int osm_finalizer();
+int osm_finalizer(){
+}
 
 
 /* Time measurement function for a simple arithmetic operation.
@@ -50,7 +42,14 @@ double osm_operation_time(unsigned int iterations);
    returns time in nano-seconds upon success,
    and -1 upon failure.
    */
-double osm_function_time(unsigned int iterations);
+double osm_function_time(unsigned int iterations){
+	struct timeval begin, after;
+	gettimeofday(&begin,NULL);
+	double time;
+
+	gettimeofday(&after,NULL);
+	return time;
+}
 
 
 /* Time measurement function for an empty trap into the operating system.
@@ -80,6 +79,19 @@ typedef struct {
 timeMeasurmentStructure measureTimes (unsigned int operation_iterations,
                                       unsigned int function_iterations,
                                       unsigned int syscall_iterations,
-                                      unsigned int disk_iterations);
+                                      unsigned int disk_iterations){
+	timeMeasurmentStructure time;
+	time.machineName[255];
+	if (gethostname(time.machineName, 254) == -1){
+		std::cout << "Problem" << std::endl;
+	}
+	time.functionTimeNanoSecond = osm_function_time(function_iterations);
+	time.instructionTimeNanoSecond = osm_operation_time(operation_iterations);
+	time.trapTimeNanoSecond = osm_syscall_time(syscall_iterations);
+	time.diskTimeNanoSecond = osm_disk_time(disk_iterations);
+	time.functionInstructionRatio = time.functionTimeNanoSecond/time.instructionTimeNanoSecond;
+	time.trapInstructionRatio = time.trapTimeNanoSecond/time.instructionTimeNanoSecond;
+	time.diskInstructionRatio = time.diskInstructionRatio/time.instructionTimeNanoSecond;
+	return time;
+}
 
-#endif
